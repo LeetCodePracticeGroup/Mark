a22ad609f440496d02dc1cb3ded31e2613be708b
Lin <xxice520@hotmail.com>
2017-09-25 22:14:12 +0800

    rename file

---
+++ easy/14-Longest_Common_Prefix.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+char* longestCommonPrefix(char** strs, int strsSize) {
+    
+}
+*/
+
+int main(){
+	char *words = {"HI", "HIHI"}; 
+	printf("%s", words[0]);
+	
+}
